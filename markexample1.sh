160966bce08167d393ebeddddc23ddeef660aaed
Mark Aksen <maksen@princeton.edu>
2016-07-19 17:49:17 -0400

    We iterate over all subjects in the project, updating data_config file with subject name and project name

--- markexample1.sh
+++ markexample1.sh
@@ -3,9 +3,12 @@
 cd "$1"
 files=`ls `
 
+mkdir /home/maksen/data_config/
 for subject in $files
 do
     echo $subject  
+    sed -e "s/50002/$subject/;s/ABIDE/$2/;s/Pitt/$2/" /home/maksen/CPAC_script.git/data_config.yml > /home/maksen/data_config/data_config_$subject.yml
+
 done
 
 
