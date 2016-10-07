74d9449692d69280e1d3ffb2baad5cbb52d3f8a7
Kevin Lin <kevinl1@andrew.cmu.edu>
2016-10-07 11:53:04 -0400

    Made the files to work

--- runCPAC.cmd
+++ runCPAC.cmd
@@ -1,3 +1,8 @@
 #!/bin/bash
+# parallel job using 1 processor and runs for 8 hours (max)
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -t 6:00:00
+# Execute commands
 
- LD_LIBRARY_PATH=/tigress/HANLIU/mridata/lib64 PYTHONPATH=$PYTHONPATH:/tigressdata/HANLIU/mridata/CPAC /usr/bin/python2.7 cpac_run.py data_config_INDI/data_config_$arg1.yml subjectlistoutput/CPAC_subject_list_$arg1.yml 
+LD_LIBRARY_PATH=/tigress/HANLIU/mridata/lib64 PYTHONPATH=$PYTHONPATH:/tigressdata/HANLIU/mridata/CPAC /usr/bin/python2.7 /tigress/HANLIU/mridata/CPAC/cpac_run.py /tigress/HANLIU/mridata/CPAC/data_config_INDI/data_config_$arg1.yml /tigress/HANLIU/mridata/CPAC/subjectlistoutput/CPAC_subject_list_$arg1.yml 
