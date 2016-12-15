990dce6b79e4c9d70cbcdb0975de118bee8d4ff7
Kevin Lin <kevinlin@nat-oitwireless-inside-vapornet100-b-30612.Princeton.EDU>
2016-12-14 21:57:28 -0500

    Added apply_parcellation

---
+++ neuroAnalysis/neuroAnalysis/R/apply_parcellation.R
@@ -0,0 +1,16 @@
+apply_parcellation <- function(arr, parcel_arr){
+  stopifnot(all(dim(arr)[1:3] == dim(parcel_arr)))
+  stopifnot(length(dim(arr)) == 4)
+
+  idx <- sort(unique(parcel_arr)); idx <- idx[idx != 0]
+  mat <- matrix(0, dim(arr)[4], length(idx))
+
+  for(i in 1:length(idx)){
+    loc <- which(parcel_arr == idx[i])
+    arr_subset <- t(apply(arr, 4, function(x){x[loc]}))
+    non_zero <- apply(arr_subset, 2, function(x){sum(abs(x))})
+    mat[,i] <- apply(arr_subset[,non_zero != 0], 1, mean)
+  }
+
+  mat
+}
