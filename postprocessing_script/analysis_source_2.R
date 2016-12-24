3c5a649c3cfdb19c8f5ea8efd8223de5815c3be9
markaksen <maksen@princeton.edu>
2016-12-23 19:24:47 -0500

    Add files via upload

---
+++ postprocessing_script/analysis_source_2.R
@@ -0,0 +1,13 @@
+parcellation <- function(dat, parcel) {
+  library(oro.nifti)
+  idx <- sort(unique(as.numeric(parcel))); idx <- idx[idx != 0]
+  timeseries <- matrix(0, dim(dat)[4], length(idx))
+  
+  for (i in 1:length(idx)) {
+    loc = which(parcel==i)
+    dat_subset <- t(apply(dat, 4, function(x){x[loc]}))
+    non_zero <- apply(dat_subset, 2, function(x){sum(abs(x))})
+    timeseries[,i] <- apply(dat_subset[,non_zero != 0, drop = F], 1, mean)
+  }
+  timeseries
+}
