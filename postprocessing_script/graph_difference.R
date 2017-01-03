1edfe13abc04ae64949d1a6928cb57f2d6fa3262
markaksen <maksen@princeton.edu>
2017-01-02 19:49:41 -0500

    Add files via upload

---
+++ postprocessing_script/graph_difference.R
@@ -0,0 +1,23 @@
+#' Graph difference
+#'
+#' @param mat1 graph encoded as sparse matrix
+#' @param mat2 graph encoded as sparse matrix
+#'
+#' @return The percentage difference and list of edges in one graph but not the other
+#' @export
+graph_difference <- function(mat1, mat2){
+  idx <- which(mat1 != mat2)
+
+  perc_diff <- length(idx)/(length(unique(c(which(mat1 != 0), which(mat2 != 0)))))
+
+  idx_pos <- which(mat1 > mat2, arr.ind = T)
+  idx_neg <- which(mat1 < mat2, arr.ind = T)
+
+  x = list(percentage_diff = perc_diff, list1 = .prune_idx(idx_pos),
+       list2 = .prune_idx(idx_neg))
+	write.table(as.data.frame(x),file="/home/maksen/CPAC_script.git/postprocessing_script/graphresult.csv", quote=F,sep=",",row.names=F)
+}
+
+.prune_idx <- function(mat){
+  mat <- mat[mat[,1] > mat[,2],]
+}
