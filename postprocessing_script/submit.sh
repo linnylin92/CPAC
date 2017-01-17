2ba6d8c72fad56cc09e9d437a4ed93976e45145b
Mark Aksen <maksen@princeton.edu>
2017-01-16 19:08:31 -0500

    fixed some minor bugs

--- postprocessing_script/submit.sh
+++ postprocessing_script/submit.sh
@@ -9,14 +9,15 @@ if [ -z "$1" ]
     then
       echo "ERROR: First argument (file path to directory with only subjects) not supplied"
       exit
+fi
 
 #run commands
 for subject in $files
 
 do
-	mkdir "$2"/"$subject"
+    mkdir "$2"/"$subject"
     cd /tigress/HANLIU/mridata/CPAC_logs/
-	sbatch --export arg1=$subject arg2=$1 arg3=$2 -o /tigress/HANLIU/mridata/CPAC_logs/slurm.%N.%j.out -e /tigress/HANLIU/mridata/CPAC_logs/slurm.%N.%j.out $location/analysis.cmd
+    sbatch --export arg1=$subject arg2=$1 arg3=$2 -o /tigress/HANLIU/mridata/CPAC_logs/slurm.%N.%j.out -e /tigress/HANLIU/mridata/CPAC_logs/slurm.%N.%j.out $location/analysis.cmd
 done
 
 #set file permissions correctly
